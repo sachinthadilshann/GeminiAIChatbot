64ab3ce2f5868b674ac9cc006fecd99316860b52
Sachintha Dilshan <sachinthadilshann@gmail.com>
2024-03-06 14:38:28 +0530

    chat bot

--- gemini_uitility.py
+++ gemini_uitility.py
@@ -2,7 +2,6 @@ import os
 import json
 import google.generativeai as ga
 
-
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 config_file_path = f"{working_dir}/config.json"
@@ -25,4 +24,9 @@ def gemini_pro_vision_captioning(prompt, image):
     return result
 
 
+def gemini_pro_response(user_prompt):
+    gemini_pro_model = ga.GenerativeModel("gemini-pro")
+    response = gemini_pro_model.generate_content(user_prompt)
+    result = response.text
+    return result
 
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from streamlit_option_menu import option_menu
 from gemini_uitility import *
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
-# print(working_dir)
 
 st.set_page_config(
     page_title="Gemini AI",
@@ -13,17 +12,16 @@ st.set_page_config(
 )
 
 with st.sidebar:
-    selected = option_menu("Gemini AI",
-                           ["Chatbot",
-                            "Image Captioning",
-                            "Image Captioning",
-                            "Embed text",
-                            "Ask me anything"])
+    selected = option_menu("Gemini AI", [
+        "Chatbot",
+        "Image Captioning",
+        "Ask Me Anything",
+    ])
 
 
-def translate_role_for_stremlit(user_role):
+def translate_role_for_streamlit(user_role):
     if user_role == "model":
-        return "assistant"
+        return "Assistant"
     else:
         return user_role
 
@@ -32,38 +30,52 @@ if selected == "Chatbot":
     model = load_gemini_pro_model()
 
     if "Chat Session" not in st.session_state:
-        st.session_state.chat_session = model.start_chat(
-            history=[]
-        )
+        st.session_state.chat_session = model.start_chat(history=[])
 
     st.title("Chatbot")
 
-    for massage in st.session_state.chat_session.history:
-        with st.chat_message(translate_role_for_stremlit(massage.role)):
-            st.markdown(massage.parts[0].text)
+    for message in st.session_state.chat_session.history:
+        with st.chat_message(translate_role_for_streamlit(message.role)):
+            st.markdown(message.parts[0].text)
 
     user_prompt = st.chat_input("User: ")
     if user_prompt:
-        st.chat_message("user").markdown(user_prompt)
+        st.chat_message("User").markdown(user_prompt)
         gemini_response = st.session_state.chat_session.send_message(user_prompt)
 
         with st.chat_message("assistant"):
             st.markdown(gemini_response.text)
 
 if selected == "Image Captioning":
-    
-    st.title("Image Captioning")
-    
-    upload_image = st.file_uploader("Upload Image....", type=["jpg", "jpeg", "png"])
-    
-    if st.button("Generate Caption"):
-        image = Image.open(upload_image)
-        coll1,coll2 = st.columns(2)
-        with coll1:
-            resize_image = image.resize((800, 500))
-            st.image(resize_image)
-        dafault_prompt = "Write a caption for this image"
-        caption = gemini_pro_vision_captioning(dafault_prompt, image)
 
-        with coll2:
-            st.write(caption)
+    st.title("Image Captioning")
+
+    uploaded_image = st.file_uploader("Upload Image...", type=["jpg", "jpeg", "png"])
+
+    if st.button("Generate Caption"):
+        if uploaded_image is not None:
+            try:
+                image = Image.open(uploaded_image)
+                coll1, coll2 = st.columns(2)
+                with coll1:
+                    resize_image = image.resize((800, 500))
+                    st.image(resize_image)
+                default_prompt = "Write a caption for this image"
+                caption = gemini_pro_vision_captioning(default_prompt, image)
+
+                with coll2:
+                    st.write(caption)
+            except Exception as e:
+                st.error(f"Error processing image: {e}")
+        else:
+            st.warning("Please upload an image first.")
+
+if selected == "Ask me anything":
+
+    st.title("❓ Ask me a question")
+
+    user_prompt = st.text_area(label='', placeholder="Ask me anything...")
+
+    if st.button("Get Response"):
+        response = gemini_pro_response(user_prompt)
+        st.markdown(response)
